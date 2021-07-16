ada68177e92f1fd0e8ab5b486d65a7f45e6ab5fb
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-07-16 11:52:52 +0430

    init

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// NOTE: Enable popovers everywhere
+
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
